551e76760487692ddbb489774de9a8fe277508ea
atabbaa1 <tabbaa.abdulrahman1@hotmail.com>
2025-06-29 10:53:12 -0400

    Changed the versionCode to 2.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.abdulrahmantabbaa.commuterpartner"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
